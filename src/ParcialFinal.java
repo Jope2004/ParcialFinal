94d455b6bd31d3e424c1613037128ba33345e94d
Juan Pablo <juanpablomartinezromero6@gmail.com>
2023-11-03 07:50:33 -0500

    final

--- src/ParcialFinal.java
+++ src/ParcialFinal.java
@@ -1,12 +1,14 @@
+
 import java.util.Scanner;
 import java.io.*;
 import java.nio.file.*;
 
 public class ParcialFinal {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] weekDays = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
-        String[][] data = new String[100][5]; 
+        String[][] data = new String[7][100];
 
         for (int i = 0; i < 7; i++) {
             String dia = weekDays[i];
@@ -33,18 +35,40 @@ public class ParcialFinal {
             System.out.println("4. Para ver datos");
             System.out.println("5. Salir");
 
-            int Opción = scanner.nextInt();
-            scanner.nextLine(); 
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
 
-            switch (Opción) {
+            switch (opcion) {
                 case 1:
-                    
+                    double maxValor = Double.MIN_VALUE;
+                    String diaMaxValor = "";
+
+                    for (int i = 0; i < 7; i++) {
+                        for (int j = 0; j < data[i].length; j++) {
+                            if (data[i][j] != null) {
+                                String[] parts = data[i][j].split(";");
+                                if (parts.length >= 2) {
+                                    double valor = Double.parseDouble(parts[1]);
+                                    if (valor > maxValor) {
+                                        maxValor = valor;
+                                        diaMaxValor = weekDays[i];
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (!diaMaxValor.isEmpty()) {
+                        System.out.println("El día de la semana con el mayor valor de transacción es " + diaMaxValor);
+                        System.out.println("El valor máximo es: " + maxValor);
+                    }
                     break;
                 case 2:
-                   
+                    findMaxColumn2Value(data);
                     break;
                 case 3:
-                    
+                    System.out.print("Ingrese un ID para buscar información: ");
+                    String idToFind = scanner.nextLine();
                     break;
                 case 4:
                     System.out.println("Datos disponibles:");
@@ -66,5 +90,29 @@ public class ParcialFinal {
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
             }
         }
+    }
+
+    private static void findMaxColumn2Value(String[][] data) {
+        double maxColumn2Value = Double.MIN_VALUE;
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                if (data[i][j] != null) {
+                    String[] parts = data[i][j].split(";");
+                    if (parts.length >= 5) {
+                        try {
+                            double column2Value = Double.parseDouble(parts[4]);
+                            if (column2Value > maxColumn2Value) {
+                                maxColumn2Value = column2Value;
+                            }
+                        } catch (NumberFormatException e) {
+                            // Ignore invalid values in column 2
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("El mayor valor en la columna 2 es: " + maxColumn2Value);
     }
 }
